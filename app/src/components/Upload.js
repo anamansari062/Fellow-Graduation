65eb188c73d9854c439c601905924c05313d808d
Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>
2022-06-03 16:28:43 +0530

    added upload form to handle files
    Signed-off-by: Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>

--- app/src/components/Upload.js
+++ app/src/components/Upload.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React,{ useState } from "react";
 
 export default function Upload() {
+
+    const [github, setGithub] = useState("");
+    const [image, setImage] = useState(null)
+
+    const submitForm = () => {}
     return (
-        <main>
-            <section >
-                <h1 >
-                    Home Page!!
-                </h1>
-            </section>
-        </main>
+        <form>
+            <input 
+                type="text"
+                value={github}
+                onChange={(e)=> setGithub(e.target.value)}
+            />
+            <br/>
+            <label>
+            <input type="file"/>
+            <span>+</span>
+            </label>
+            <br/>
+            <button onClick={submitForm}>Submit</button>
+        </form>
     )
 }
